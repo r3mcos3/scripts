a205ea808f5f86701d323c08300b2a38e51a6c45
r3mcos3 <r3mcos3@gmail.com>
2023-06-18 10:24:19 +0200

    🎉 feat(ssh.sh): add script to ssh and mount/unmount remote servers This commit adds a script that allows the user to ssh into two remote servers and mount/unmount their shares. The script provides a menu with options to choose from. The options include ssh-ing into the Home Assistant server, ssh-ing into the Unraid server, mounting the Home Assistant share, mounting the Unraid server, unmounting the Home Assistant share, unmounting the Unraid server, and unmounting all shares. The script is designed to improve productivity by automating the process of ssh-ing and mounting/unmounting remote servers.

---
+++ ssh/ssh.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+function ssh_ha {
+    ssh root@10.10.50.149
+    exit 0
+}
+
+function ssh_unraid {
+    ssh -p 2224 root@10.10.50.60
+    exit 0
+}
+
+function mnt_ha {
+    sshfs root@10.10.50.149:/config /home/remco/homeassistant
+    exit 0
+}
+
+function mnt_unraid {
+    sshfs -p 2224 root@10.10.50.60:/mnt/user /home/remco/unraid
+    exit 0
+}
+
+function umnt_ha {
+    umount /home/remco/homeassistant
+    exit 0
+}
+
+function umnt_unraid {
+    umount /home/remco/unraid
+    exit 0
+}
+
+function umnt_all {
+    umount /home/remco/homeassistant && umount /home/remco/unraid
+    exit 0
+}
+
+while :
+do
+    echo "Menu"
+    echo "1. Ssh To Home Assistant"
+    echo "2. Ssh To Unraid"
+    echo "3. Mount Home Assistant Share"
+    echo "4. Mount Unraid Server"
+    echo "5. Umount Home Assistant Share"
+    echo "6. Umount Unraid Server"
+    echo "7. Unmount All"
+    echo "8. Exit"
+    read -p "Make Your Choice:" choice
+
+    case $choice in
+    1)
+        ssh_ha;;
+    2)
+        ssh_unraid;;
+    3)
+        mnt_ha;;
+    4)
+        mnt_unraid;;
+    5)
+        umnt_ha;;
+    6)
+        umnt_unraid;;
+    7)
+        umnt_all;;
+    8)
+        echo "exiting..."
+        exit 0
+        ;;
+    *)
+        echo "Invalid Choice Please Try Again"
+        ;;
+    esac
+done
